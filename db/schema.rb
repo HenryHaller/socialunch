df6997568cb378c4ed5120178e2382d62fedbe75
Henry Haller <henrybhaller@gmail.com>
2018-08-21 11:11:33 +0900

    add basic models and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_091536) do
+ActiveRecord::Schema.define(version: 2018_08_21_012008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "lunch_dates", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "second_user_id"
+    t.bigint "restaurant_id"
+    t.datetime "begin"
+    t.integer "suggested_duration"
+    t.string "lunch_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_lunch_dates_on_restaurant_id"
+    t.index ["second_user_id"], name: "index_lunch_dates_on_second_user_id"
+    t.index ["user_id"], name: "index_lunch_dates_on_user_id"
+  end
+
+  create_table "lunch_requests", force: :cascade do |t|
+    t.datetime "datetime"
+    t.integer "suggested_duration"
+    t.string "lunch_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "name"
+    t.text "description"
+    t.string "cuisine"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "birthday"
+    t.boolean "smoking"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "lunch_dates", "restaurants"
 end

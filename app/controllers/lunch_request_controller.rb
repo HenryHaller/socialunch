b738262059823b88db159e124720c7e3e7f0b08c
Henry Haller <henrybhaller@gmail.com>
2018-08-21 16:05:10 +0900

    ready to test controller implementation

--- app/controllers/lunch_request_controller.rb
+++ app/controllers/lunch_request_controller.rb
@@ -4,6 +4,13 @@ class LunchRequestController < ApplicationController
   end
 
   def create
+    @lunch_request = LunchRequest(lunch_request_params)
+    @lunch_request.user = current_user
+    if @lunch_request.save
+      redirect_to LunchRequest_show_path(@lunch_request)
+    else
+      render :new
+    end
   end
 
   def show
@@ -14,5 +21,10 @@ class LunchRequestController < ApplicationController
     lunch_request = LunchRequest.find(params[:id])
     lunch_request.destroy
     redirect_to root_path
+  end
+
+  private
+  def lunch_request_params
+    params.require(:lunch_request).permit(:datetime, :suggested_duration, :lunch_type)
   end
 end

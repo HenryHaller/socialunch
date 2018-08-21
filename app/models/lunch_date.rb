5245ef2518b1fae224776717f54c1bca9cd96c2e
Henry Haller <henrybhaller@gmail.com>
2018-08-21 12:06:28 +0900

    This gets the seed file working and the lunch date model working well enough to run the seed file.

--- app/models/lunch_date.rb
+++ app/models/lunch_date.rb
@@ -1,4 +1,5 @@
 class LunchDate < ApplicationRecord
   belongs_to :user
+  belongs_to :second_user
   belongs_to :restaurant
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,42 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Restaurant.delete_all
+User.delete_all
+LunchRequest.delete_all
+LunchDate.delete_all
 
-puts "making locations 6 restaurants"
+
+
+puts "making restaurants"
 
 6.times do |variable|
-  Restaunt.create()
+  Restaurant.create(address: Faker::Address.full_address,
+  latitude: Faker::Address.latitude, longitude: Faker::Address.longitude,
+  name: Faker::Pokemon.name, description: Faker::TheITCrowd.quote,
+  cuisine: Faker::Superhero.power, phone_number: Faker::PhoneNumber.cell_phone)
 end
+
+puts "making users "
+
+6.times do |variable|
+  User.create(first_name: Faker::Dog.name,
+    last_name: Faker::Cat.name, birthday: Faker::Date.birthday(18, 65), smoking: [true, false].sample,
+    bio: Faker::TheFreshPrinceOfBelAir.quote)
+end
+
+puts "making lunch requests"
+6.times do
+  LunchRequest.create(datetime: Time.now, suggested_duration: [30, 60, 90, 120].sample, lunch_type: ["casual", "professional"].sample)
+end
+
+puts "making lunch dates"
+6.times do |variable|
+  LunchDate.create(user: User.all.sample, second_user: User.all.sample, restaurant: Restaurant.all.sample,
+    begin: Time.now, suggested_duration: 60, lunch_type: ["casual", "professional"].sample)
+end
+
+"Made #{Restaurant.count} restaurants"
+"Made #{User.count} users"
+"Made #{LunchRequest.count} lunch requests"
+"Made #{LunchDate.count} lunch dates"

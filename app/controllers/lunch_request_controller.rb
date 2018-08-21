f98f8000eb2ae8c14adbac2ff982103693888db5
Henry Haller <henrybhaller@gmail.com>
2018-08-21 15:58:59 +0900

    first round of controller implementation

--- app/controllers/lunch_request_controller.rb
+++ app/controllers/lunch_request_controller.rb
@@ -1,13 +1,18 @@
 class LunchRequestController < ApplicationController
   def new
+    @lunch_request = LunchRequest.new
   end
 
   def create
   end
 
   def show
+    LunchRequest.find(params[:id])
   end
 
   def destroy
+    lunch_request = LunchRequest.find(params[:id])
+    lunch_request.destroy
+    redirect_to root_path
   end
 end

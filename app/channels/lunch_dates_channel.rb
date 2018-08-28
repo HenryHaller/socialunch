ace6a6087c1083f92571e5e5f72449b8d6cdab94
Henry Haller <henrybhaller@gmail.com>
2018-08-28 14:16:54 +0900

    kind of a start

--- app/channels/lunch_dates_channel.rb
+++ app/channels/lunch_dates_channel.rb
@@ -1,6 +1,6 @@
-class ChatRoomsChannel < ApplicationCable::Channel
+class LunchDatesChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "chat_room_#{params[:id]}"
+    stream_from "lunch_date_#{params[:id]}"
     # stream_from "some_channel"
   end
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -8,7 +8,11 @@ class Message < ApplicationRecord
   end
 
   def broadcast_message
-    ActionCable.server.broadcast("lunch_date_#{lunch_date.id}", {
+    channel = "lunch_date_#{lunch_date.id}"
+    puts ">>>>>>>>>>>>>>>>>>"
+    puts channel
+    puts "<<<<<<<<<<<<<<<<<<"
+    ActionCable.server.broadcast(channel, {
       message_partial: ApplicationController.renderer.render(partial: "messages/message", locals: { message: self, user_is_messages_author: false }),
       current_user_id: user.id
     })

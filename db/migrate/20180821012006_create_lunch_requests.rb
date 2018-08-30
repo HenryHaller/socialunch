23683afae69abc56fe9daa0836d84866617a47da
Ableen Tai <ableen@gmail.com>
2018-08-30 16:47:19 +0900

    clean up file for heroku so lunch request game works

--- db/migrate/20180821012006_create_lunch_requests.rb
+++ db/migrate/20180821012006_create_lunch_requests.rb
@@ -6,7 +6,7 @@ class CreateLunchRequests < ActiveRecord::Migration[5.2]
       t.string :lunch_type
       t.references :user
       t.references :lunch_date
-      t.integer :score_increase
+      t.integer :score_increase, default: 0
 
       t.timestamps
     end

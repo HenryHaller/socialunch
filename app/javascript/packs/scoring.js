d9499dbc400c02e19439c2fbabf74aac1602d747
Henry Haller <henrybhaller@gmail.com>
2018-08-30 20:35:22 +0900

    add some animations for the get points

--- app/javascript/packs/scoring.js
+++ app/javascript/packs/scoring.js
@@ -1,13 +1,25 @@
-// const choiceButtons = document.querySelectorAll(".choice_button");
-// console.log(choiceButtons);
-// const score_form = document.querySelector("#score_form");
-// console.log(score_form);
-// choiceButtons.forEach((btn) => {
-//   console.log(btn);
-//   btn.addEventListener('click', (event)=>{
-//     // console.log(event);
-//     event.preventDefault();
-//     score_form.submit();
-//   })
+console.log("doing scoring.js")
 
-// });
+const pointsButtons = document.querySelectorAll(".points-button");
+
+const handleClick = (event) => {
+  console.log(event.currentTarget);
+  event.currentTarget.style.opacity = "0.0";
+  pointsButtons.forEach( (pointButton) => {
+    if (pointButton !== event.currentTarget) {
+      pointButton.style.opacity = "0.0";
+    };
+  });
+
+};
+
+// const handleMouseover = (event) => {
+//   console.log(event.currentTarget)
+//   event.currentTarget.innerText = "Choose me!";
+// };
+
+pointsButtons.forEach( (pointsButton) => {
+  // console.log(pointsButton);
+  pointsButton.addEventListener('click', handleClick);
+  // pointsButton.addEventListener('mouseover', handleMouseover);
+});

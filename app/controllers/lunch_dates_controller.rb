3c0f3b57d2357eb72b88d25eb937b3f5e0167e57
Henry Haller <henrybhaller@gmail.com>
2018-08-31 11:01:46 +0900

    basic index page

--- app/controllers/lunch_dates_controller.rb
+++ app/controllers/lunch_dates_controller.rb
@@ -1,4 +1,15 @@
 class LunchDatesController < ApplicationController
+  def index
+    puts current_user
+    @lunch_dates = []
+    LunchDate.all.each do |date|
+      puts date
+      if date.request1.user == current_user || date.request2.user == current_user
+        @lunch_dates << date
+      end
+    end
+  end
+
   def show
     @lunch_date = LunchDate.includes(messages: :user).find(params[:id])
     authorize @lunch_date
